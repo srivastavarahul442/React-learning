9a780d3777261eb1eae76c5eb52f2e7a5d0ae906
srivastavarahul442 <srivastavarahul442@gmail.com>
2023-12-20 19:38:11 +0530

    E-1

---
+++ App.js
@@ -0,0 +1,25 @@
+const parent = React.createElement(
+    "div", { id: "parent" }, [
+        React.createElement(
+            "div", { id: "child1" },[
+            React.createElement("h1", {}, "hi... i am h1 tag"),
+            React.createElement("h2", {}, "hi... i am h2 tag")
+            ]
+        ),React.createElement(
+            "div", { id: "child2" },[
+            React.createElement("h1", {}, "hi... i am h1 tag"),
+            React.createElement("h2", {}, "hi... i am h2 tag")
+            ]
+        )
+    ]
+)
+
+const body = ReactDOM.createRoot(document.getElementById("body"));
+body.render(parent);
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+
+root.render(heading);
